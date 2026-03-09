1f3cc00a51158b305bd66fa68ea5f9beac28acde
mcprox <mehmetcopur_01@outlook.com>
2026-03-09 14:49:05 +0100

    nav2 launch file 3

--- ros2_ws/src/robot_bringup/launch/nav2.launch.py
+++ ros2_ws/src/robot_bringup/launch/nav2.launch.py
@@ -1,8 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -10,6 +9,7 @@ import os
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
+    map_file = LaunchConfiguration('map')
 
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     robot_bringup_dir = get_package_share_directory('robot_bringup')
@@ -18,6 +18,12 @@ def generate_launch_description():
         robot_bringup_dir,
         'config',
         'nav2_params.yaml'
+    )
+
+    default_map_file = os.path.join(
+        robot_bringup_dir,
+        'maps',
+        'map.yaml'
     )
 
     nav2_launch = os.path.join(
@@ -35,11 +41,16 @@ def generate_launch_description():
             'params_file',
             default_value=default_params_file
         ),
+        DeclareLaunchArgument(
+            'map',
+            default_value=default_map_file
+        ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(nav2_launch),
             launch_arguments={
                 'use_sim_time': use_sim_time,
-                'params_file': params_file
+                'params_file': params_file,
+                'map': map_file
             }.items()
         )
     ])
